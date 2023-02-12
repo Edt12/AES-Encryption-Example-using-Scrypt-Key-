84b9ce22817b1d60236fd1aecae290765ed7707a
Edt12 <104518243+Edt12@users.noreply.github.com>
2023-02-12 10:46:31 +0000

    Add files via upload

--- Encryption test.py
+++ Encryption test.py
@@ -2,7 +2,10 @@ import os
 from cryptography.hazmat.primitives.kdf.scrypt import Scrypt
 from cryptography.hazmat.primitives.ciphers import Cipher,algorithms,modes
 
-
+#1.Generate Key using Scrypt
+#2.Make Cypher out of Key
+#3.Make Decryptor out of Cypher to Decrypt
+#4.Make Encryptor out of Cypher to Encrypt-Before it is encrypted data must be encoded 
 def GenerateKey(UsernameAndPassword):
     salt=os.urandom(16)
     iv=os.urandom(16)
@@ -23,9 +26,9 @@ def Decrypt(Data):
 def Encrypt(Data):
     Encryptor=cipher.encryptor()
     Data=Data.encode()
-    EncryptedData=Encryptor.update(Data)+Encryptor.finalize()
+    EncryptedData=Encryptor.update(Data)+Encryptor.finalize()#update encrypts finalize returns what it has encrypted same for decrypt
     return EncryptedData
 
-Steve=Encrypt(Data="A secret message")
+Steve=Encrypt(Data="A secret message")#Has to be multiple of 128 for AES encryption to work will find a solution to this later
 
 print(Decrypt(Steve))
