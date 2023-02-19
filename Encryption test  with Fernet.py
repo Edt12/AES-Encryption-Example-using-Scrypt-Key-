19c3a2a4a5d8c9570be5d5b1de1575013fd644f3
Edt12 <104518243+Edt12@users.noreply.github.com>
2023-02-19 18:03:45 +0000

    fun

--- Encryption test  with Fernet.py
+++ Encryption test  with Fernet.py
@@ -7,13 +7,13 @@ from cryptography.fernet import Fernet
 #2.Put Key into Fernet
 #padding built in 
 def GenerateKey(UsernameAndPassword):
-    salt=os.urandom(16)
+    salt=os.urandom(32)
     KeyDerivationFunction=Scrypt(salt=salt,length=32,n=2**20,r=8,p=1)
     UsernameAndPassword=str(UsernameAndPassword).encode()
     Key=base64.urlsafe_b64encode(KeyDerivationFunction.derive(UsernameAndPassword))
     cipher=Fernet(Key)
     return cipher
-cipher=GenerateKey(UsernameAndPassword="steve")
+cipher=GenerateKey(UsernameAndPassword=os.urandom(128))
 
 def Decrypt(Data):
     DecryptedData=cipher.decrypt(Data)
@@ -24,6 +24,6 @@ def Encrypt(Data):
     EncryptedData=cipher.encrypt(Data)
     return EncryptedData
 
-Steve=Encrypt(Data="A  message")#Has to be multiple of 128 for AES encryption to work will find a solution to this later
+Steve=Encrypt(Data="I LOVE IT WHEN PEOPLE DONT UNDERSTAND WHAT IM SAYING")#Has to be multiple of 128 for AES encryption to work will find a solution to this later
 print(Steve)
 print(Decrypt(Steve))
